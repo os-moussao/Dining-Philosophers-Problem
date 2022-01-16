4c451e00b114bd2d2158e3b3bddbb2cff8b871ed
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-01-16 18:31:10 +0100

    update multiple_threads_in_a_loop.c

--- Processes & Threads/multiple_threads_in_a_loop.c
+++ Processes & Threads/multiple_threads_in_a_loop.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+int bin[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
 
 void	*routine(void *ptr)
 {
-	printf("Start of thread\n");
-	sleep(1);
-	printf("End of thread\n");
+	printf("%d  ", bin[*(int *)ptr]);
 	return ptr;
 }
 
 int main(int ac, char **av)
 {
-	pthread_t th[4];
+	pthread_t th[10];
+	int *arr[10];
 	
-	for (int i=0; i < 4; i++) {
-		if (pthread_create(&th[i], NULL, routine, NULL) != 0) {
+	for (int i=0; i < 10; i++) {
+		arr[i] = malloc(sizeof(int));
+		*(arr[i]) = i;
+		if (pthread_create(&th[i], NULL, routine, arr[i]) != 0) {
 			printf("Error\n");
 			return 1;
 		}
 	}
 	
-	for (int i=0; i < 4; i++) {
+	for (int i=0; i < 10; i++) {
+		free(arr[i]);
 		if (pthread_join(th[i], NULL) != 0) {
 			printf("Error\n");
 			return 2;
